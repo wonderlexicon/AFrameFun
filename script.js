3715af09934b972d0165b45200ed0250df75984b
wonderlexicon <nh756@nyu.edu>
2020-12-05 12:30:37 -0500

    continued to experiment with interactivity, added circle planes, second dodecahedron under water

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 console.log("me");
-let cubeElem = document.getElementById("cube");
-cubeElem.addEventListener("mouseenter", function () {
-    cubeElem.setAttribute("scale", "3 3 3");
-})
+// let cubeElem = document.getElementById("cube");
+// cubeElem.addEventListener("mouseenter", function () {
+//     cubeElem.setAttribute("scale", "3 3 3");
+// })
