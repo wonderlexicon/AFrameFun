0d874f64621e37c8bed28e2ebd56db10e599aa39
wonderlexicon <nh756@nyu.edu>
2020-12-07 18:48:34 -0500

    created interactive camera zoom element

--- script.js
+++ script.js
@@ -1,5 +1,21 @@
 console.log("me");
-// let cubeElem = document.getElementById("cube");
-// cubeElem.addEventListener("mouseenter", function () {
-//     cubeElem.setAttribute("scale", "3 3 3");
-// })
+
+window.addEventListener("wheel", event=>{
+    let myCamera = document.getElementById("camera");
+    const delta=Math.sign(event.wheelDelta);
+    const currentZoom = Number(myCamera.getAttribute("zoom"));
+    let newZoom = delta * 0.05 + currentZoom;
+   
+    const closestZoom = 1; 
+    const farthestZoom =0.01;
+
+    if (newZoom > closestZoom){
+        newZoom= closestZoom
+    }
+    if (newZoom < farthestZoom){
+        newZoom = farthestZoom
+    }
+
+    myCamera.setAttribute("zoom", newZoom);
+    console.log({newZoom})
+})
