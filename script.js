385e5beeb5dbc5069651e7263aa0153831642470
wonderlexicon <nh756@nyu.edu>
2020-12-07 23:02:26 -0500

    added new teture imgs

--- script.js
+++ script.js
@@ -4,9 +4,9 @@ window.addEventListener("wheel", event=>{
     let myCamera = document.getElementById("camera");
     const delta=Math.sign(event.wheelDelta);
     const currentZoom = Number(myCamera.getAttribute("zoom"));
-    let newZoom = delta * 0.05 + currentZoom;
+    let newZoom = delta * 0.1 + currentZoom;
    
-    const closestZoom = 1; 
+    const closestZoom = 5; 
     const farthestZoom =0.01;
 
     if (newZoom > closestZoom){
